42fb5f7c0c4dee723dab1972be02d1924d5fc9e3
haykam821 <budderblockss+2@gmail.com>
2017-04-11 14:43:17 -0400

    Got rid of the logging

--- fontbot.js
+++ fontbot.js
@@ -47,7 +47,6 @@ function parseToEmoji(cmd, message) {
 
     } else {
       for (var i = 0; i < array.length; i++) {
-          console.log(font['emojis'][array[i]])
           array[i] = font['emojis'][array[i]] == undefined ? array[i] : font['emojis'][array[i]]
       }
     }
